f75b86997e076590eca04d71c020740ccfa11a0d
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-05-03 18:51:22 +0530

    css file type added

--- backend/services/django/myapp/settings.py
+++ backend/services/django/myapp/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 import environ
 import os
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
