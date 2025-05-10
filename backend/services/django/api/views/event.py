965e83492844d67e3f6ff464b81143532a93474e
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-05-10 14:25:30 +0530

    build file and url update

--- backend/services/django/api/views/event.py
+++ backend/services/django/api/views/event.py
@@ -1,4 +1,3 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -6,9 +5,6 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.reverse import reverse
 from datetime import datetime
 
-from api.models.category import Category
-from api.models.subcategory import SubCategory
-from api.models.media import Media
 from api.models.event import Event
 
 class EventViewSet(ViewSet):
@@ -16,42 +12,6 @@ class EventViewSet(ViewSet):
         return Response({
             'event_by_date': request.build_absolute_uri(reverse('event-event_by_date', kwargs={'date': '24-04-2025'}, request=request)),
         })
-
-
-        grouped = []
-
-        categories = Category.objects.all().order_by('order', 'id')
-
-        for category in categories:
-            cat_data = {
-                "category": category.name,                
-                "subcategories": []
-            }
-
-            subcategories = SubCategory.objects.filter(category=category).order_by('name')
-
-            for subcategory in subcategories:
-                media_items = Media.objects.filter(
-                    categories=category,
-                    subcategories=subcategory
-                ).distinct()[:10]  # limit to 10 items
-
-                cat_data["subcategories"].append({
-                    "name": subcategory.name,
-                    "media": [
-                        {
-                            "id": item.id,
-                            "title": item.title,
-                            "image": f"{request.scheme}://{request.get_host()}/{item.media}"
-
-                        }
-                        for item in media_items 
-                    ]
-                })
-
-            grouped.append(cat_data)
-
-        return Response(grouped)
 
     @action(detail=False, methods=['get'], url_path=r'(?P<date>\d{2}-\d{2}-\d{4})', url_name='event_by_date')
     def event_by_date(self, request, date):
@@ -75,7 +35,7 @@ class EventViewSet(ViewSet):
                     'media': [
                         {
                             'id': m.id,
-                            'url': f"{request.scheme}://{request.get_host()}/uploads/{m.media}",
+                            'url': m.image,
                             'type': m.media_type
                         } for m in media
                     ]
